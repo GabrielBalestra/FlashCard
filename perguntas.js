1dbb4e06154c3b25f370c3be9d364cbd38e6a8ac
GabrielBalestra <146239343+GabrielBalestra@users.noreply.github.com>
2024-10-22 10:06:44 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
+    'jogo de açao',
+',
     'O Python é uma linguagem de programação'
 )
 
 criaCartao(
-    'Geografia',
+    'jogo de açao',
     'Qual a capital da França?',
     'A capital da França é Paris'
 )
 
 criaCartao(
-    'Programação',
+    'jogo de  açao',
     'O que é uma função?',
     'Uma função é um bloco de código que executa alguma tarefa'
 )
 
 criaCartao(
-    'Lingua inglesa',
+    'jogo de açao',
     'Como se diz oi em Inglês?',
     'Oi em ingles é HI (RAI)'
 )
