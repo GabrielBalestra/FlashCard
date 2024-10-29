a0c2de8ffefd36b296ff0e1ffc51a8fc54487e08
GabrielBalestra <146239343+GabrielBalestra@users.noreply.github.com>
2024-10-29 09:44:01 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    'jogo de açao',
-    '. Qual é o objetivo principal no jogo Free Fire?'
-    'O objetivo principal no Free Fire é ser o último jogador ou equipe sobrevivente em uma partida de battle royale. Para isso, os jogadores precisam coletar recursos, armas e equipamentos enquanto eliminam os adversários e evitam ficar fora da zona segura.'
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
 )
 
 criaCartao(
-    'jogo de açao',
+    'Geografia',
     'Qual a capital da França?',
     'A capital da França é Paris'
 )
 
 criaCartao(
-    'jogo de  açao',
+    'Programação',
     'O que é uma função?',
     'Uma função é um bloco de código que executa alguma tarefa'
 )
 
 criaCartao(
-    'jogo de açao',
+    'Lingua inglesa',
     'Como se diz oi em Inglês?',
     'Oi em ingles é HI (RAI)'
 )
